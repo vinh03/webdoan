641272d134e44dc94f3824f0e4e496a11ccb62d1
vinh03 <vinh09092003dn@gmail.com>
2024-05-07 08:23:31 +0700

    update cor

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(bodyParser.raw({ type: 'application/json' }));
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}))
 app.use(fileUpload({
     useTempFiles: true,
 }))
